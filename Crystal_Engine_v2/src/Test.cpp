da72fca66e19f21ead9110ea0b5aaa49ebab86ac
Ricardo <Ricardo@DESKTOP-62OPED7>
2019-02-11 23:23:15 +0100

    Project setup

---
+++ Crystal_Engine_v2/src/Test.cpp
@@ -0,0 +1,11 @@
+#include "Test.h"
+#include <iostream>
+
+using namespace std;
+
+namespace crystal {
+
+	void print() {
+		cout << "Hello crystal!" << endl;
+	}
+}
---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,9 @@
+
+namespace crystal {
+	
+	__declspec(dllimport) void print();
+}
+
+void main() {
+	crystal::print();
+}
