b1cc9cb2f0290ed8ef0e42e45c6ef4ec3e30738c
Ricardo <Ricardo@DESKTOP-62OPED7>
2019-02-23 19:25:50 +0100

    Log done

--- Crystal_Engine_v2/src/crystal/EntryPoint.h
+++ Crystal_Engine_v2/src/crystal/EntryPoint.h
@@ -7,9 +7,10 @@ extern Crystal::Application* Crystal::CreateApplication();
 int main(int argc, char** argv)
 {
 	Crystal::Log::Init();
-	Crystal::Log::GetCoreLogger()->warn("Initialized log!");
-	Crystal::Log::GetClientLogger()->info("Hello!");
 
+	CRYSTAL_CORE_WARN("Initialized log!");
+	int a = 5;
+	CRYSTAL_INFO("Hello var={0}", a);
 
 	auto sandbox = Crystal::CreateApplication();
 	sandbox->Run();
--- Crystal_Engine_v2/src/crystal/Log.h
+++ Crystal_Engine_v2/src/crystal/Log.h
@@ -22,3 +22,15 @@ namespace Crystal {
 		static shared_ptr<logger> coreLogger;
 	};
 }
+
+#define CRYSTAL_CORE_ERROR(...)	Crystal::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define CRYSTAL_CORE_WARN(...)	Crystal::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define CRYSTAL_CORE_INFO(...)	Crystal::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define CRYSTAL_CORE_TRACE(...) Crystal::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define CRYSTAL_CORE_FATAL(...) Crystal::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+#define CRYSTAL_ERROR(...)	Crystal::Log::GetClientLogger()->error(__VA_ARGS__)
+#define CRYSTAL_WARN(...)	Crystal::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define CRYSTAL_INFO(...)	Crystal::Log::GetClientLogger()->info(__VA_ARGS__)
+#define CRYSTAL_TRACE(...)	Crystal::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define CRYSTAL_FATAL(...)	Crystal::Log::GetClientLogger()->fatal(__VA_ARGS__)
