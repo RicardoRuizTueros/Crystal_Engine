85a5981a1ff9bf3f4043bd053295c09c42371645
ricardo <ricardo.ruiz.tueros@icloud.com>
2019-06-02 12:54:17 +0200

    Windows window implementation updated

---
+++ Crystal_Engine_v2/src/platform/windows/WindowsWindow.cpp
@@ -0,0 +1,67 @@
+#include "crystalpch.h"
+#include "WindowsWindow.h"
+
+namespace Crystal 
+{
+	static bool GLFWInitialized = false;
+
+	Window* Window::Create(const WindowProperties& properties)
+	{
+		return new WindowsWindow(properties);
+	}
+
+	WindowsWindow::WindowsWindow(const WindowProperties& properties)
+	{
+		Init(properties);
+	}
+
+	WindowsWindow::~WindowsWindow()
+	{
+		Shutdown();
+	}
+
+	void WindowsWindow::Init(const WindowProperties& properties)
+	{
+		data.title = properties.title;
+		data.width = properties.width;
+		data.height = properties.height;
+
+		CRYSTAL_CORE_INFO("Creating window {0} ({1}, {2})", properties.title, properties.width, properties.height);
+
+		if (!GLFWInitialized)
+		{
+			// TODO: glfwTerminate on system shutdown
+			int success = glfwInit();
+			CRYSTAL_CORE_ASSERT(success, "Could not intialize GLFW!");
+
+			GLFWInitialized = true;
+		}
+
+		window = glfwCreateWindow((int)properties.width, (int)properties.height, properties.title.c_str(), nullptr, nullptr);
+		glfwMakeContextCurrent(window);
+		glfwSetWindowUserPointer(window, &data);
+		SetVSync(true);
+	}
+
+	void WindowsWindow::Shutdown()
+	{
+		glfwDestroyWindow(window);
+	}
+
+	void WindowsWindow::OnUpdate()
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	void WindowsWindow::SetVSync(bool enabled)
+	{
+		glfwSwapInterval(enabled);
+		data.vSync = enabled;
+	}
+
+	bool WindowsWindow::IsVSync() const
+	{
+		return data.vSync;
+	}
+}
---
+++ Crystal_Engine_v2/src/platform/windows/WindowsWindow.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "crystal/Window.h"
+#include <GLFW/glfw3.h>
+
+namespace Crystal {
+
+	class WindowsWindow : public Window
+	{
+	public:
+		WindowsWindow(const WindowProperties& properties);
+		virtual ~WindowsWindow();
+
+		void OnUpdate() override;
+
+		inline unsigned int GetWidth() const override { return data.width; }
+		inline unsigned int GetHeight() const override { return data.height; }
+
+		inline void SetEventCallback(const EventCallbackFunction& callback) override { data.callback = callback; }
+		void SetVSync(bool enabled) override;
+		bool IsVSync() const override;
+
+	private:
+		struct WindowData
+		{
+			string title;
+			unsigned int width, height;
+			bool vSync;
+			EventCallbackFunction callback;
+		};
+
+		GLFWwindow* window;
+		WindowData data;
+
+		virtual void Init(const WindowProperties& properties);
+		virtual void Shutdown();
+	};
+}
