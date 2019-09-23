c0d6c9d0a2252d6d94dc79fbefa92c91f42356e0
Ricardo <Ricardo@DESKTOP-4VC6503>
2019-09-23 19:06:47 +0200

    Added OpenGLShader

---
+++ Crystal_Engine/src/platform/openGL/OpenGLShader.cpp
@@ -0,0 +1,173 @@
+#include "crystalpch.h"
+#include "OpenGLShader.h"
+
+#include <glad/glad.h>
+
+#include <glm/gtc/type_ptr.hpp>
+
+using namespace std;
+
+namespace Crystal
+{
+	OpenGLShader::OpenGLShader(const string& vertexSource, const string& fragmentSource)
+	{
+		// Create an empty vertex shader handle
+		GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+		// Send the vertex shader source code to GL
+		// Note that std::string's .c_str is NULL character terminated.
+		const GLchar* source = vertexSource.c_str();
+		glShaderSource(vertexShader, 1, &source, 0);
+
+		// Compile the vertex shader
+		glCompileShader(vertexShader);
+
+		GLint isCompiled = 0;
+		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &isCompiled);
+		if (isCompiled == GL_FALSE)
+		{
+			GLint maxLength = 0;
+			glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &maxLength);
+
+			// The maxLength includes the NULL character
+			vector<GLchar> infoLog(maxLength);
+			glGetShaderInfoLog(vertexShader, maxLength, &maxLength, &infoLog[0]);
+
+			// We don't need the shader anymore.
+			glDeleteShader(vertexShader);
+
+			CRYSTAL_CORE_ERROR("{0}", infoLog.data());
+			CRYSTAL_CORE_ASSERT(false, "Vertex shader compilation failure!");
+
+			return;
+		}
+
+		// Create an empty fragment shader handle
+		GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+		// Send the fragment shader source code to GL
+		// Note that std::string's .c_str is NULL character terminated.
+		source = fragmentSource.c_str();
+		glShaderSource(fragmentShader, 1, &source, 0);
+
+		// Compile the fragment shader
+		glCompileShader(fragmentShader);
+
+		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &isCompiled);
+		if (isCompiled == GL_FALSE)
+		{
+			GLint maxLength = 0;
+			glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &maxLength);
+
+			// The maxLength includes the NULL character
+			std::vector<GLchar> infoLog(maxLength);
+			glGetShaderInfoLog(fragmentShader, maxLength, &maxLength, &infoLog[0]);
+
+			// We don't need the shader anymore.
+			glDeleteShader(fragmentShader);
+			// Either of them. Don't leak shaders.
+			glDeleteShader(vertexShader);
+
+			CRYSTAL_CORE_ERROR("{0}", infoLog.data());
+			CRYSTAL_CORE_ASSERT(false, "Fragment shader compilation failure!");
+
+			return;
+		}
+
+		// Vertex and fragment shaders are successfully compiled.
+		// Now time to link them together into a program.
+		// Get a program object.
+		rendererID = glCreateProgram();
+
+		// Attach our shaders to our program
+		glAttachShader(rendererID, vertexShader);
+		glAttachShader(rendererID, fragmentShader);
+
+		// Link our program
+		glLinkProgram(rendererID);
+
+		// Note the different functions here: glGetProgram* instead of glGetShader*.
+		GLint isLinked = 0;
+		glGetProgramiv(rendererID, GL_LINK_STATUS, (int*)& isLinked);
+		if (isLinked == GL_FALSE)
+		{
+			GLint maxLength = 0;
+			glGetProgramiv(rendererID, GL_INFO_LOG_LENGTH, &maxLength);
+
+			// The maxLength includes the NULL character
+			std::vector<GLchar> infoLog(maxLength);
+			glGetProgramInfoLog(rendererID, maxLength, &maxLength, &infoLog[0]);
+
+			// We don't need the program anymore.
+			glDeleteProgram(rendererID);
+			// Don't leak shaders either.
+			glDeleteShader(vertexShader);
+			glDeleteShader(fragmentShader);
+
+			CRYSTAL_CORE_ERROR("{0}", infoLog.data());
+			CRYSTAL_CORE_ASSERT(false, "Shader link failure!");
+			return;
+		}
+
+		// Always detach shaders after a successful link.
+		glDetachShader(rendererID, vertexShader);
+		glDetachShader(rendererID, fragmentShader);
+	}
+	
+	OpenGLShader::~OpenGLShader()
+	{
+		glDeleteProgram(rendererID);
+	}
+	
+	void OpenGLShader::Bind() const
+	{
+		glUseProgram(rendererID);
+	}
+	
+	void OpenGLShader::Unbind() const
+	{
+		glUseProgram(0);
+	}
+
+	void OpenGLShader::UploadUniformInt(const string& name, int value)
+	{
+		GLint location = glGetUniformLocation(rendererID, name.c_str());
+		glUniform1i(location, value);
+	}
+
+	void OpenGLShader::UploadUniformFloat(const string& name, float value)
+	{
+		GLint location = glGetUniformLocation(rendererID, name.c_str());
+		glUniform1f(location, value);
+	}
+
+	void OpenGLShader::UploadUniformFloat2(const string& name, const vec2& vector)
+	{
+		GLint location = glGetUniformLocation(rendererID, name.c_str());
+		glUniform2f(location, vector.x, vector.y);
+	}
+
+	void OpenGLShader::UploadUniformFloat3(const string& name, const vec3& vector)
+	{
+		GLint location = glGetUniformLocation(rendererID, name.c_str());
+		glUniform3f(location, vector.x, vector.y, vector.z);
+	}
+
+	void OpenGLShader::UploadUniformFloat4(const string& name, const vec4& vector)
+	{
+		GLint location = glGetUniformLocation(rendererID, name.c_str());
+		glUniform4f(location, vector.x, vector.y, vector.z, vector.w);
+	}
+	
+	void OpenGLShader::UploadUniformMat3(const string& name, const mat4& matrix)
+	{
+		GLint location = glGetUniformLocation(rendererID, name.c_str());
+		glUniformMatrix3fv(location, 1, GL_FALSE, value_ptr(matrix));
+	}
+
+	void OpenGLShader::UploadUniformMat4(const string& name, const mat3& matrix)
+	{
+		GLint location = glGetUniformLocation(rendererID, name.c_str());
+		glUniformMatrix4fv(location, 1, GL_FALSE, value_ptr(matrix));
+	}
+}
---
+++ Crystal_Engine/src/platform/openGL/OpenGLShader.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "crystal/renderer/Shader.h"
+#include <glm/glm.hpp>
+
+using namespace std;
+using namespace glm;
+
+namespace Crystal
+{
+	class OpenGLShader : public Shader
+	{
+	public:
+		OpenGLShader(const string& vertexSource, const string& fragmentSource);
+		~OpenGLShader();
+
+		void Bind() const override;
+		void Unbind() const override;
+
+		void UploadUniformInt(const string& name, int value);
+
+		void UploadUniformFloat(const string& name, float value);
+		void UploadUniformFloat2(const string& name, const vec2& vector);
+		void UploadUniformFloat3(const string& name, const vec3& vector);
+		void UploadUniformFloat4(const string& name, const vec4& vector);
+
+		void UploadUniformMat3(const string& name, const mat4& matrix);
+		void UploadUniformMat4(const string& name, const mat3& matrix);
+
+	private:
+		uint32_t rendererID;
+	};
+}
+
