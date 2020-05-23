d1fa99253a056ab1192b55ccaf9f199e9daad8d8
RicardoRuizTueros <ricardo.ruiz.tueros@icloud.com>
2020-05-23 15:54:57 +0200

    Updated spdlog

--- Crystal_Engine/vendor/spdlog/out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Ricky/source/repos/Crystal_Engine/Crystal_Engine/vendor/spdlog
-# Build directory: C:/Users/Ricky/source/repos/Crystal_Engine/Crystal_Engine/vendor/spdlog/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("example")
-subdirs("tests")
